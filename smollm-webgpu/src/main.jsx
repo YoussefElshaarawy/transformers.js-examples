ffb789a3532e410fa13792c3424834faff192c0c
Youssef Elshaarawy <61767977+YoussefElshaarawy@users.noreply.github.com>
2025-05-30 19:36:18 +0300

    Update main.jsx

--- smollm-webgpu/src/main.jsx
+++ smollm-webgpu/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "./univer.js"; 
 
 const chatRoot = document.getElementById("chat-root");
 if (!chatRoot) {
